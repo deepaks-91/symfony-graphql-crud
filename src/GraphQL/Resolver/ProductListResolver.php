9c74bb57cc79bb8e900c5fc268589b053550f2c0
deepakaug20 <deepak.malangi@gmail.com>
2020-05-05 14:29:11 +0530

    first commit

---
+++ src/GraphQL/Resolver/ProductListResolver.php
@@ -0,0 +1,29 @@
+<?php
+namespace App\GraphQl\Resolver;
+
+
+use Doctrine\ORM\EntityManager;
+use Overblog\GraphQLBundle\Definition\Argument;
+use Overblog\GraphQLBundle\Definition\Resolver\AliasedInterface;
+use Overblog\GraphQLBundle\Definition\Resolver\ResolverInterface;
+
+class ProductResolver implements ResolverInterface, AliasedInterface
+{
+    private $em;
+    public function __construct(EntityManager $em)
+    {
+        $this->em = $em;
+    }
+
+    public function resolve(Argument $argument)
+    {
+        return $this->em->getRepository('App:ProductListing')->find($argument['id']);
+    }
+
+    public static function getAliases(): array
+    {
+        return [
+            'resolve' => 'Product'
+        ];
+    }
+}
